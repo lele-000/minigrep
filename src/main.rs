29e03724743e223ddf7a636fdba8bb366219e6b6
bigfish <1219128243@qq.com>
2023-04-29 20:27:08 +0800

    add

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let (query, file_path) = parse_config(&args);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+    (query, file_path)
+}
