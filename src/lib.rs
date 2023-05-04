73a66bec210b0abceccd879990b26f758dc1736c
bigfish <1219128243@qq.com>
2023-05-04 15:43:00 +0800

    add

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,6 @@
 use std::error::Error;
 use std::fs;
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-    Ok(())
-}
-
 pub struct Config {
     pub query: String,
     pub file_path: String,
@@ -22,5 +15,39 @@ impl Config {
         let file_path = args[2].clone();
 
         Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    for line in search(&config.query, &contents) {
+        println!("{line}")
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe,fast,production.
+Pick three.";
+        assert_eq!(vec!["safe,fast,production."], search(query, contents));
     }
 }
